30aed09a9a221d2624e89a2c2dfb16ec200d7705
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-01-06 11:23:22 +0300

    сделана страница портфеля

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Headline from "./components/Headline/Headline";
 import Investment from "./components/Investment/Investment";
 import Transaction from "./components/Transaction/Transaction";
 import Calm from "./components/Сlaim/Calm";
+import Products from "./components/Products/Products";
 
 import Header from "./components/Header/Header";
 import MainCabinet from "./components/MainCabinet/MainCabinet";
 import CabinetHeadline from "./components/CabinetHeadline/CabinetHeadline";
 import Profile from "./components/Profile/Profile";
+import Lots from "./components/Lots/Lots";
+import Report from "./components/Report/Report";
 
 function App() {
   return (
@@ -24,28 +27,44 @@ function App() {
       <Calm />
       <Footer />*/}
 
-      {/*страница кабинет
-      <div className="Cabinet">
+      {/*страница кабинет*/}
+      <div className="Profiles">
         <Header />
         <MainCabinet />
-        <div className="MarginFooter">
+        <div className="MarginTop80">
+          <Footer />
+        </div>
+      </div>
+
+      {/*профиль
+      <div className="Profiles">
+        <Header /> 
+        <div className="Profiles__wrapper">
+          <CabinetHeadline />
+          <Profile />
+        </div>
+        <Footer />
+      </div>*/}
+
+      {/*портфель
+
+      <div className="Profiles">
+        <Header />
+        <div className="MainCabinet__wrapper">
+          <CabinetHeadline />
+          <div className="MainCabinet__block MarginTop30">
+            <Products />
+            <div className="MarginBottom20">
+              <Report />
+            </div>
+          </div>
+
+          <Lots />
+        </div>
+        <div className="MarginTop90">
           <Footer />
         </div>
       </div>*/}
-
-      {/*профиль*/}
-      <div className="Profiles">
-        <Header />
-        <div className="Profiles__wrapper">
-        <CabinetHeadline />
-        <Profile />
-
-        </div>
-
-      
-        
-        <Footer />
-      </div>
     </div>
   );
 }
---
+++ src/components/Lots/Lots.jsx
@@ -0,0 +1,138 @@
+import "./Lots.css";
+import clock from "../../assets/images/clock.png";
+
+function Lots() {
+  return (
+    <div className="Lots">
+      <h3>Проинвестированные лоты</h3>
+
+      <div className="Lots__block">
+        <div className="Lots__list">
+          <img src={clock} alt="clock" />
+
+          <div className="Lots__lot width-big">
+            <h6>Лот 1</h6>
+            <h3>Антикварные часы</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Статус</h6>
+            <h3>Завершен</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>В работе</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Свободные</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+        </div>
+
+        <div className="Products__left-card-buttonsBlock">
+          <button>Вывести</button>
+          <button>Внести</button>
+        </div>
+      </div>
+      <hr />
+
+      <div className="Lots__block">
+        <div className="Lots__list">
+          <img src={clock} alt="clock" />
+
+          <div className="Lots__lot width-big">
+            <h6>Лот 1</h6>
+            <h3>Антикварные часы</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Статус</h6>
+            <h3>Завершен</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>В работе</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Свободные</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+        </div>
+
+        <div className="Products__left-card-buttonsBlock">
+          <button>Вывести</button>
+          <button>Внести</button>
+        </div>
+      </div>
+      <hr />
+      <div className="Lots__block">
+        <div className="Lots__list">
+          <img src={clock} alt="clock" />
+
+          <div className="Lots__lot width-big">
+            <h6>Лот 1</h6>
+            <h3>Антикварные часы</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Статус</h6>
+            <h3>Завершен</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>В работе</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Свободные</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+        </div>
+
+        <div className="Products__left-card-buttonsBlock">
+          <button>Вывести</button>
+          <button>Внести</button>
+        </div>
+      </div>
+      <hr />
+
+      <div className="Lots__block">
+        <div className="Lots__list">
+          <img src={clock} alt="clock" />
+
+          <div className="Lots__lot width-big">
+            <h6>Лот 1</h6>
+            <h3>Антикварные часы</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Статус</h6>
+            <h3>Завершен</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>В работе</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+
+          <div className="Lots__lot width">
+            <h6>Свободные</h6>
+            <h3>75 000 ₽</h3>
+          </div>
+        </div>
+
+        <div className="Products__left-card-buttonsBlock">
+          <button>Вывести</button>
+          <button>Внести</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Lots;
