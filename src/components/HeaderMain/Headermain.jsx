8cd021e116ab8b1c0eab24348035cd4e4e101ebb
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-01-18 17:20:14 +0300

    исправлена вёрска на больших разрешениях

--- src/components/HeaderMain/Headermain.jsx
+++ src/components/HeaderMain/Headermain.jsx
@@ -1,19 +1,23 @@
 import needle from "../../assets/images/botton-needle.png";
-import Logo from '../Logo/Logo'
-import "../../index.css"
-
-
+import Logo from "../Logo/Logo";
+import "../../index.css";
 import "./headerMain.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Cabinet from "../Cabinet";
 
 function HeaderMain() {
   return (
     <div className="App">
-        <header className="header">
-          <Logo />
-          <button>Войти
-            <img className="needle" src={needle}/>
+      <header className="header">
+        <Logo />
+   
+          <button className="header__button">
+            Войти
+            <img className="needle" src={needle} />
           </button>
-        </header>
+     
+      </header>
     </div>
   );
 }
